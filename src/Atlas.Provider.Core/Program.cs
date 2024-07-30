56f4fbb2021570862ac38777832fd620c211d190
Giau. Tran Minh <hello@giautm.dev>
2024-07-30 15:11:23 +0000

    src/core: simplify executor with interested operations

--- src/Atlas.Provider.Core/Program.cs
+++ src/Atlas.Provider.Core/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Atlas.Provider.Core.Executor;
 
 namespace Atlas.Provider.Core
 {
@@ -17,47 +18,42 @@ namespace Atlas.Provider.Core
       bool nullable = true
     )
     {
-      using var executor = new ReflectionOperationExecutor(
-        assembly: assembly,
-        startupAssembly: startupAssembly,
-        projectDir: projectDir,
-        dataDirectory: null,
-        rootNamespace: rootNamespace,
-        language: language,
-        nullable: nullable,
-        remainingArguments: null
+      using var executor = new EFDesign(
+        assembly,
+        startupAssembly,
+        projectDir,
+        null,
+        rootNamespace,
+        language,
+        nullable,
+        []
       );
-
       var types = executor.GetContextTypes();
-
       foreach (var type in types)
       {
         if (!type.Contains("Name") || type["Name"] == null)
         {
           continue;
         }
-
         var name = type["Name"]!.ToString();
         if (string.IsNullOrEmpty(name))
         {
           continue;
         }
-
-        var sql = executor.ScriptDbContext(name);
         var ctxInfo = executor.GetContextInfo(name);
-
         if (ctxInfo == null || !ctxInfo.Contains("ProviderName") || ctxInfo["ProviderName"] == null)
         {
           continue;
         }
-
-        var providerName = ctxInfo["ProviderName"]!.ToString();
-        if (providerName!.EndsWith("SqlServer"))
+        var sql = executor.ScriptDbContext(name);
+        if (ctxInfo["ProviderName"]!.ToString()!.EndsWith("SqlServer"))
         {
           sql = "-- atlas:delimiter GO \n" + sql;
         }
-
-        Console.WriteLine(sql ?? string.Empty);
+        if (!string.IsNullOrEmpty(sql))
+        {
+          Console.WriteLine(sql ?? string.Empty);
+        }
       }
     }
   }
