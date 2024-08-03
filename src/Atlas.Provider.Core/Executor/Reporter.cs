cd4a48b49c276b2c6efefa6ebe0a6c73914682ad
luantranminh <tranminhluan1206@gmail.com>
2024-08-03 16:19:59 +0700

    refactor: adhere to stdout/stderr conventions

--- src/Atlas.Provider.Core/Executor/Reporter.cs
+++ src/Atlas.Provider.Core/Executor/Reporter.cs
@@ -1,44 +1,39 @@
 using System;
+using System.CommandLine;
 using System.Linq;
 
 namespace Atlas.Provider.Core.Executor;
 
 internal static class Reporter
 {
-    public static bool IsVerbose { get; set; }
-    public static bool PrefixOutput { get; set; }
+  public static bool IsVerbose { get; set; }
+  public static bool PrefixOutput { get; set; }
 
-    public static void WriteError(string? message)
-        => WriteLine(Prefix("error:   ", message));
+  public static void WriteError(string? message)
+      => Console.Error.WriteLine(Prefix("error:   ", message));
 
-    public static void WriteWarning(string? message)
-        => WriteLine(Prefix("warn:    ", message));
+  public static void WriteWarning(string? message)
+      => Console.Error.WriteLine(Prefix("warn:    ", message));
 
-    public static void WriteInformation(string? message)
-        => WriteLine(Prefix("info:    ", message));
+  public static void WriteInformation(string? message)
+      => Console.WriteLine(Prefix("info:    ", message));
 
-    public static void WriteData(string? message)
-        => WriteLine(Prefix("data:    ", message));
+  public static void WriteData(string? message)
+      => Console.WriteLine(Prefix("data:    ", message));
 
-    public static void WriteVerbose(string? message)
+  public static void WriteVerbose(string? message)
+  {
+    if (IsVerbose)
     {
-        if (IsVerbose)
-        {
-            WriteLine(Prefix("verbose: ", message));
-        }
+      Console.WriteLine(Prefix("verbose: ", message));
     }
+  }
 
-    private static string? Prefix(string prefix, string? value)
-        => PrefixOutput
-            ? value == null
-                ? prefix
-                : string.Join(
-                    Environment.NewLine,
-                    value.Split(new[] { Environment.NewLine }, StringSplitOptions.None).Select(l => prefix + l))
-            : value;
-
-    private static void WriteLine(string? value)
-    {
-        Console.WriteLine(value);
-    }
+  private static string? Prefix(string prefix, string? value)
+      => 
+          value == null
+              ? prefix
+              : string.Join(
+                  Environment.NewLine,
+                  value.Split(new[] { Environment.NewLine }, StringSplitOptions.None).Select(l => prefix + l));
 }
